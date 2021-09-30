1cbd1de2510eb49cde18bbe62ac40e079c57a5f9
Sonja Parsell <hello@sparsell.com>
2021-09-30 13:05:19 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+export default class EyesOnMe extends React.Component {
+    onBlur = (event) => {
+        console.log("Hey! Eyes on me!")
+    }
+
+    onFocus = (event) => {
+        console.log("Good!")
+    }
+    render() {
+        return (
+            <button onBlur={this.onBlur} onFocus={this.onFocus}></button>
+        )
+    }
+
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,15 @@
 // Code Keypad Component Here
+import React from 'react'
+
+export default class Keypad extends React.Component {
+    handleKeyUp = (event) => {
+        console.log('Entering password...')
+    }
+
+    render() {
+        return (
+            <input onKeyUp={this.handleKeyUp} type='password'/>
+        )
+    }
+
+}
